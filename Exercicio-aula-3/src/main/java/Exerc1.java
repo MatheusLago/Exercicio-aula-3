d8e2462be83f930180bbe007a530405cafbef6f8
Matheus Lago <matheuslagoband@hotmail.com>
2022-03-03 10:38:27 -0300

    Resolvendo o Exericio1

---
+++ Exercicio-aula-3/src/main/java/Exerc1.java
@@ -0,0 +1,14 @@
+public class Exerc1 {
+
+    public static void main(String[] args) {
+
+        int zumb1 = 120;
+        int zumb2 = 240;
+        int zumb3 = 320;
+
+        int total = zumb1 + zumb2 zumb3;
+
+        System.out.println("Total de Zumbis :" + total);
+        System.out.printf("Media de Zumbis :" + total/3);
+    }
+}

5a0bdb932791251b1e312936147488c19f07b7e8
Matheus Lago <matheuslagoband@hotmail.com>
2022-03-03 19:38:20 -0300

    Resolvendo exer2

--- Exercicio-aula-3/src/main/java/Exerc1.java
+++ Exercicio-aula-3/src/main/java/Exerc1.java
@@ -6,7 +6,7 @@ public class Exerc1 {
         int zumb2 = 240;
         int zumb3 = 320;
 
-        int total = zumb1 + zumb2 zumb3;
+        int total = zumb1 + zumb2 + zumb3;
 
         System.out.println("Total de Zumbis :" + total);
         System.out.printf("Media de Zumbis :" + total/3);
---
+++ Exercicio-aula-3/src/main/java/Exerc2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exerc2 {
+
+    public static void main(String[] args) {
+
+        Scanner valorTeclado = new Scanner(System.in);
+        System.out.println("Numeros de zumbis primeira partida");
+        int valor1 = valorTeclado.nextInt();
+        System.out.println("Numeros de zumbis segunda partida");
+        int valor2 = valorTeclado.nextInt();
+        System.out.println("Numeros de zumbis terceira partida");
+        int valor3 = valorTeclado.nextInt();
+        int total = valor1 + valor2 +valor3;
+
+
+        System.out.println("Total de Zumbis: " + total);
+        System.out.println("Média de Zumbis: " + total/3);
+    }
+
+}
